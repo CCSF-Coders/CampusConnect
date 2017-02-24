c37b60030e6ece77aafbdd4f2ec745509c3721ff
MiLeung <michaeleleung@gmail.com>
2017-02-24 00:35:22 -0800

    google login kinda...

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
+
 import './index.css';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="Container">
-        <h1> CCSF Connect </h1>
-        <h3> Discover, Interact, and Explore </h3>
-        <h4> An app created for CCSF Clubs </h4>
+        <h1>CCSF Connect</h1>
+        <h3>Discover, Interact, and Explore</h3>
+        <p>An app created for CCSF Clubs</p>
+        <div className="g-signin2" data-onsuccess={() => {console.log('signed in!')}}></div>
       </div>
     );
   }
