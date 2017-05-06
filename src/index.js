d4abfb9941cd9749356a48f75e277e83774b3649
MiLeung <michaeleleung@gmail.com>
2017-05-06 11:09:02 -0700

    hot reloading

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+const root = document.getElementById('root');
+
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  root
 );
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    const NextApp = require('./components/App').default
+    ReactDOM.render(
+      <NextApp />,
+      root
+    )
+  })
+}
